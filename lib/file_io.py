7617b47ecea9911abcbbc7c5ba87a3d5971f8a8f
Salehe <salehe.mkomwa@student.moringaschool.com>
2025-05-26 09:34:59 +0300

    half done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,10 @@
 def write_file(file_name, file_content):
-    pass
+    file_name = str(file_name) + ".txt"
+    with open(file_name, 'w') as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_name = str(file_name) + ".txt"
+    with open(file_name, "a") as file:
+        file.write(append_content)
 
-def read_file(file_name):
-    pass
